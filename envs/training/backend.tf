490924e2f77201371475611e91c01783a01c3f16
dinhbach <dinhbach@kms-technology.com>
2026-06-04 10:51:01 +0700

    feat: configure Terraform backend and add CI workflow for training environment

--- envs/training/backend.tf
+++ envs/training/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg-commercial-trainning"
-#     storage_account_name = "sttfstatetraining"
-#     container_name       = "tfstate"
-#     key                  = "training.tfstate"
-#     use_azuread_auth     = true
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-commercial-trainning"
+    storage_account_name = "stcommerciala9be68"
+    container_name       = "tfstate"
+    key                  = "training.tfstate"
+    use_azuread_auth     = true
+  }
+}
